e60ec6ee8c6f7cc160ecf1200436c799fb571a07
Erez A. Korn <erakorn@outlook.com>
2014-01-28 12:25:49 +0200

    Added BooleanToVisibilityConverter

---
+++ Portable/YoctoMvvmWindowsStore/Common/BooleanToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace YoctoMvvmWindowsStore.Common {
+    public class BooleanToVisibilityConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return (value is bool && (bool)value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException("Only implementing Convert");
+        }
+    }
+}
