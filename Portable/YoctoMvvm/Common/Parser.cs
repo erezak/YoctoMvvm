f52a606a5e683ac1f9eebdab64e9c8dbc67d7389
Erez A. Korn <erakorn@outlook.com>
2014-02-04 03:16:56 +0200

    Added IsValidEmail extension method to Parser.

--- Portable/YoctoMvvm/Common/Parser.cs
+++ Portable/YoctoMvvm/Common/Parser.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Json;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace YoctoMvvm.Common {
@@ -24,6 +25,19 @@ namespace YoctoMvvm.Common {
                 var msBytes = ms.ToArray();
                 return Encoding.UTF8.GetString(msBytes, 0, msBytes.Length);
             }
+        }
+
+        public static bool IsValidEmail(this string email) {
+            if (string.IsNullOrWhiteSpace(email)) {
+                return false;
+            }
+            // hthttp://haacked.com/archive/2007/08/21/i-knew-how-to-validate-an-email-address-until-i.aspx
+            var emailPattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+                               + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+                               + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+            var emailValidator = new Regex(emailPattern, RegexOptions.IgnoreCase);
+            return emailValidator.IsMatch(email);
+
         }
     }
 }
