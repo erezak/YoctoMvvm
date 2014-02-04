3516b72e6a9d78c8574f3f751bccaabbf970c51c
Erez A. Korn <erakorn@outlook.com>
2014-02-04 03:17:35 +0200

    Fixed Dispatcher implementation for Windows Store

--- Portable/YoctoMvvmWindowsStore/Platform/DispatcherWindowsStore.cs
+++ Portable/YoctoMvvmWindowsStore/Platform/DispatcherWindowsStore.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
 using Windows.UI.Core;
+using Windows.UI.Xaml;
 using YoctoMvvm.Platform;
 
 namespace YoctoMvvmWindowsStore.Platform {
     public class DispatcherWindowsStore : IDispatcher {
         public async void RunOnUiThread(Action action) {
-            await CoreWindow.GetForCurrentThread().Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => {
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => {
                 action();
             });
         }
